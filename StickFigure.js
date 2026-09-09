cb0e4debb58304d623c0ccb7c52f39e0d2ecaf9e
keithperez <keithperezprofessional@gmail.com>
2026-09-09 10:02:15 -0400

    Initial commit

---
+++ StickFigure.js
@@ -0,0 +1,19 @@
+// found the quadcopter code in the source code of the website
+function StickFigure(context, x, y) {
+
+    this.x = x;
+    this.y = y;
+
+
+}
+
+StickFigure.prototype.draw = function() {}
+
+StickFigure.prototype.drawLeftArm = function(rotation) {}
+
+StickFigure.prototype.drawRightArm = function(rotation) {}
+
+StickFigure.prototype.drawLeftLeg = function(rotation) {}
+
+StickFigure.prototype.drawRightLeg = function(rotation) {}
+
